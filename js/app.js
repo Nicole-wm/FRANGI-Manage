122baf0c08f3c604f600d3b2a974d339906455b2
1372375047@qq.com <1372375047@qq.com>
2018-05-07 17:42:00 +0800

    make web front

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 'use strict';
 
-angular.module('app', ['ngAnimate','ngCookies','ngResource','ngSanitize','ngTouch','ngStorage','ui.router','ui.bootstrap','ui.load','ui.jq','ui.validate','oc.lazyLoad','pascalprecht.translate']);
+angular.module('app', ['ngAnimate','ngCookies','ngResource','ngSanitize','ngTouch','ngStorage','ui.router','ui.bootstrap','ui.load','ui.jq','ui.validate','oc.lazyLoad','pascalprecht.translate']).
+  filter('htmlToPlaintext', function() {
+    return function(text) {
+      return  text ? String(text).replace(/<[^>]+>/gm, '') : '';
+    };
+  }
+);
--- js/controllers/material/MateListController.js
+++ js/controllers/material/MateListController.js
@@ -3,11 +3,32 @@ app.controller('MateListCtrl', ['$scope','$modal','toaster','$stateParams',funct
 	$scope.CurTypeName = $stateParams.typeName;
 
 	$scope.MateList=[{
-		"id": "1",
+		"id": 1,
+		"cid":1,
+		"tid":1,
 		"createtime":"2018-05-01",
 		"updatetime":"2018-05-01",
 		"content":"种草好精华又不想太贵？请选择FRANGI二代女神水",
+		"publish":1,
 		"posters":["../images/mateImg/1/1.jpg","../images/mateImg/1/2.jpg","../images/mateImg/1/3.jpg"]
+	},{
+		"id":2,
+		"cid":1,
+		"tid":2,
+		"createtime":"2018-05-02",
+		"updatetime":"2018-05-03",
+		"content":"<p><b>护肤Tips</b></p>看过来~",
+		"publish":1,
+		"posters":["../images/mateImg/1/4.jpg","../images/mateImg/1/5.jpg","../images/mateImg/1/1.jpg"]
+	},{
+		"id":3,
+		"cid":2,
+		"tid":1,
+		"createtime":"2018-05-03",
+		"updatetime":"2018-05-04",
+		"content":"<p><b>【FRANGI 微商 · 开门红】</b></p><p>开好头，起好步，整年运势挡不住</p><p>活动福利三重奏</p><p>最大的优惠 + 最热卖的产品</p><p>打下全年业绩好基础</p>",
+		"publish":0,
+		"posters":["../images/mateImg/1/2.jpg","../images/mateImg/1/4.jpg"]
 	}
 	]
 
@@ -38,15 +59,96 @@ app.controller('MateListCtrl', ['$scope','$modal','toaster','$stateParams',funct
 		});
 	};
 
+	$scope.deleteMate = function (ObjID) {
+		var CurCateID=ObjID;
+		var CommonText = {
+			modalTitle: '删除素材',
+			modalContent: '确定删除该素材？'
+		};
+		var modalInstance = $modal.open({
+			templateUrl: 'myModalConfirm',
+			controller: 'ComConfirmCtrl',
+			size: '',
+			resolve: {
+				items: function () { 
+					return CommonText;
+				}
+			}
+		});
+
+		modalInstance.result.then(function (result) {
+			if(result=="ok"){
+				toaster.pop("success","","删除成功！");
+			}else{
+			}
+		}, function () {
+		});
+	};
 }]); 
 
 app.controller('MateModalCtrl', ['$scope','$modalInstance','items','FileUploader','taOptions',function($scope,$modalInstance,items,FileUploader,taOptions) {
 	$scope.CurShow=-1;
 	taOptions.toolbar = [
-		['h1', 'h2', 'h3', 'h4', 'h5', 'h6','p'],
-		['bold', 'italics', 'underline', 'ul', 'ol', 'redo', 'undo', 'clear'],
-		['justifyLeft', 'justifyCenter', 'justifyRight', 'indent', 'outdent']
+	['h1', 'h2', 'h3', 'h4', 'h5', 'h6','p'],
+	['bold', 'italics', 'underline', 'ul', 'ol', 'redo', 'undo', 'clear'],
+	['justifyLeft', 'justifyCenter', 'justifyRight', 'indent', 'outdent']
 	];
+
+	$scope.CateList = [
+	{
+		"id": 1, 
+		"cname": "女神水",
+		"type": [{
+			"id":1,
+			"tname": "女神水实拍图",
+			"createtime":"2018-05-01",
+			"updatetime":"2018-05-01",
+			"banner":"../images/typeBanner/1/1.jpg"
+		},{
+			"id":2,
+			"tname": "女神水功效海报",
+			"createtime":"2018-05-02",
+			"updatetime":"2018-05-02",
+			"banner":"../images/typeBanner/1/2.jpg"
+		},{
+			"id":3,
+			"tname": "女神水模特图",
+			"createtime":"2018-05-03",
+			"updatetime":"2018-05-03",
+			"banner":"../images/typeBanner/1/3.jpg"
+		},{
+			"id":4,
+			"tname": "女神水使用场景图",
+			"createtime":"2018-05-04",
+			"updatetime":"2018-05-04",
+			"banner":"../images/typeBanner/1/4.jpg"
+		},{
+			"id":5,
+			"tname": "买家秀",
+			"createtime":"2018-05-05",
+			"updatetime":"2018-05-05",
+			"banner":"../images/typeBanner/1/5.jpg"
+		}
+		]
+	},{
+		"id": 2, 
+		"cname": "巴厘岛",
+		"type": [{
+			"id":1,
+			"tname": "产品图",
+			"createtime":"2018-05-01",
+			"updatetime":"2018-05-01",
+			"banner":"../images/typeBanner/1/3.jpg"
+		},{
+			"id":2,
+			"tname": "经销商",
+			"createtime":"2018-05-05",
+			"updatetime":"2018-05-05",
+			"banner":"../images/typeBanner/1/2.jpg"
+		}
+		]
+	}
+	]
 
 	$scope.mate = items;
 	if(!$scope.mate){//新建
@@ -54,7 +156,20 @@ app.controller('MateModalCtrl', ['$scope','$modalInstance','items','FileUploader
 			"posters":["../img/poster.png"]
 		}
 		$scope.mate.content = '<h3>在此输入素材内容!</h3><p>例如</p><p><b>【FRANGI 微商 · 开门红】</b></p><p>开好头，起好步，整年运势挡不住</p><p>活动福利三重奏</p><p>最大的优惠 + 最热卖的产品</p><p>打下全年业绩好基础</p>';
+		$scope.mate.cid = null;
+		$scope.mate.tid = null;
 	}
+
+	$scope.ChangeCate = function () {
+		$.each($scope.CateList, function(index,value) {  
+			if ($scope.CateList[index]["id"] == $scope.mate.cid) {  
+				$scope.CurCate=$scope.CateList[index];
+				return false;  
+			}  
+		});  
+	};
+	
+	$scope.ChangeCate();
 
 	var uploader = $scope.uploader = new FileUploader({
 		url: 'js/controllers/upload.php'
@@ -79,6 +194,7 @@ app.controller('MateModalCtrl', ['$scope','$modalInstance','items','FileUploader
 	};
 
 	$scope.ok = function () {
+		console.log($scope.mate.CurCate);
 		$modalInstance.close($scope.mate);
 	};
 
